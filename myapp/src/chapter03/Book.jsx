d29c23f16c6aee824e9b974e2f6f5bd7018fe75e
dayun <rlaekdbs0329@naver.com>
2024-03-15 18:14:59 +0900

    안녕을 삭제

--- myapp/src/chapter03/Book.jsx
+++ myapp/src/chapter03/Book.jsx
@@ -6,8 +6,8 @@ function Book(props){
             <h1>{`이 책의 이름은 ${props.name}입니다.`}</h1>
             <h1>{`이 책은 총 ${props.numOfPage}페이지로 이뤄져 있습니다.`}</h1>
 
-        <div>안녕
-        </div>
+        
+
         </div>
     );
 }
